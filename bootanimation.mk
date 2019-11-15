126b98c306bad5795fad38ef15757a4d0392236f
ZVNexus <zvnexus@outlook.com>
2019-11-14 19:18:57 -0500

    New boot animation for DU 14
    Change-Id: I4130058691f860da961db38330e4ddf246f83e01

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,29 +1,13 @@
 # Boot Animation
-scr_resolution := 1080x1920
+scr_resolution := 1080
 du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter taimen,$(du_device)),)
-scr_resolution := 1440x2880
-endif
-
-ifneq ($(filter crosshatch coral,$(du_device)),)
-scr_resolution := 1440x2960
-endif
-
-ifneq ($(filter marlin,$(du_device)),)
-scr_resolution := 1440x2560
-endif
-
-ifneq ($(filter bonito sargo,$(du_device)),)
-scr_resolution := 1080x2160
-endif
-
-ifneq ($(filter dragon,$(du_device)),)
-scr_resolution := 1800x2560
+ifneq ($(filter crosshatch coral dragon marlin taimen,$(du_device)),)
+scr_resolution := 1440
 endif
 
 ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
     vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
-    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
+    vendor/themes/bootanimation/$(scr_resolution)-dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
 endif
