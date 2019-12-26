4912b8154d124738de718c3ae059315ca6e1273b
Alexander Koskovich <zvnexus@outlook.com>
2019-12-26 13:38:06 -0500

    Update accents from Android 10
     * Also allign accents with AOSP implementation.
    
    Change-Id: I4f92b7650cbc30af91e6ce8274479879a4bb2763

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@
 DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 
 # Accents
-#include vendor/themes/accents.mk
+include vendor/themes/accents.mk
 
 # Boot animation
 include vendor/themes/bootanimation.mk
